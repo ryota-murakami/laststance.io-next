ccd9d4ca44f99ef3c0268ad0c93c7ca80a11d9d4
ryota-murakami <dojce1048@gmail.com>
2023-09-22 00:10:31 +0900

    Fix syntax error in next.config.mjs and update target in tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,31 @@
 import rehypePrism from '@mapbox/rehype-prism'
+// import mdxLoader from '@mdx-js/loader'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // @FIXME SyntaxError: "undefined" is not valid JSON
+  // experimental: {
+  //   turbo: {
+  //     rules: {
+  //       '*.mdx': [
+  //         function (code) {
+  //           const prevGetOptions = this.getOptions.bind(this)
+  //           this.getOptions = function getOptions(...args) {
+  //             return {
+  //               ...prevGetOptions(...args),
+  //               rehypePlugins: [rehypePrism],
+  //               remarkPlugins: [remarkGfm],
+  //             }
+  //           }
+  //
+  //           mdxLoader.call(this, code)
+  //         },
+  //       ],
+  //     },
+  //   },
+  // },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
