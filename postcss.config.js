4d7aac93ce1ff1afdd8dfa65aa6f1faa927cabf3
ryota-murakami <dojce1048@gmail.com>
2023-09-29 01:54:34 +0900

    Update postcss.config.js to include postcss-nesting plugin and reorder plugins for better organization

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-/* eslint-disable sort-keys-custom-order/object-keys */
 module.exports = {
   plugins: {
     autoprefixer: {},
-    tailwindcss: {},
     'postcss-nesting': {},
+    tailwindcss: {},
   },
 }
