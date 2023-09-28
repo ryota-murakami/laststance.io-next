231293b185bd3815c6aaaf993832c7c6d9650c70
ryota-murakami <dojce1048@gmail.com>
2023-09-28 11:06:33 +0900

    Update FeedItem component to display feed content and published date

--- src/app/_home/FeedItem.tsx
+++ src/app/_home/FeedItem.tsx
@@ -7,13 +7,9 @@ interface Props {
 export const FeedItem: React.FC<Props> = ({ feed }) => (
   <li className="flex gap-4">
     <dl className="flex flex-auto flex-wrap gap-x-2">
-      <dt className="sr-only">Content</dt>
+      <dt className="sr-only">Feed Item</dt>
       <dd className="text-zinc-500 dark:text-zinc-400">
         <div dangerouslySetInnerHTML={{ __html: feed.content[0]._ }} />
-      </dd>
-      <dt className="sr-only">Date</dt>
-      <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
-        <time dateTime={feed.published[0]}>{feed.published[0]}</time>
       </dd>
     </dl>
   </li>
