79e82e04caf9685f74d628acc649768419d7d1fd
ryota-murakami <dojce1048@gmail.com>
2023-09-28 17:55:28 +0900

    Update metadata description in articles page to include focus on JavaScript OSS and new product

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -8,7 +8,7 @@ import { formatDate } from '@/lib/formatDate'
 export const metadata: Metadata = {
   title: 'Articles',
   description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+    'All of my long-form thoughts on JavaScript OSS, new product, and more, collected in chronological order.',
 }
 function Article({ article }: { article: ArticleWithSlug }) {
   return (
