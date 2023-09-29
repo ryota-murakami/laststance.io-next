fb0013a5f6192bd71b5a8caa1ac0564acd9eaa82
ryota-murakami <dojce1048@gmail.com>
2023-09-29 14:51:22 +0900

    Update project page with new projects and metadata

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,47 +3,83 @@ import Image from 'next/image'
 
 import { Card, CardLink, CardDescription } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import chromeLogo from '@/images/logos/icons8-chrome-48.png'
+import npmLogo from '@/images/logos/icons8-npm-48.png'
+import reactLogo from '@/images/logos/icons8-react-a-javascript-library-for-building-user-interfaces-32.png'
+import reduxLogo from '@/images/logos/icons8-redux-48.png'
+import viteLogo from '@/images/logos/icons8-vite-48.png'
 
 const projects = [
   {
-    name: 'Planetaria',
-    description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+    name: 'eslint-config-ts-prefixer',
+    description: 'Make Your Linter as Close to Prettier as Possible.',
+    link: {
+      href: 'https://github.com/laststance/eslint-config-ts-prefixer',
+      label: 'eslint-config-ts-prefixer',
+    },
+    logo: npmLogo,
   },
   {
-    name: 'Animaginary',
-    description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
+    name: 'Create React App Vite',
+    description: 'Simple CRA style Vite teimpate.',
+    link: {
+      href: 'https://github.com/laststance/create-react-app-vite',
+      label: 'Create React App Vite',
+    },
+    logo: viteLogo,
   },
   {
-    name: 'HelioStream',
+    name: 'vite-rtk-query',
     description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
+      'Vite template for React + TypeScript + Redux-Toolkit / RTK Query.',
+    link: {
+      href: 'https://github.com/laststance/vite-rtk-query',
+      label: 'vite-rtk-query',
+    },
+    logo: reduxLogo,
   },
   {
-    name: 'cosmOS',
+    name: 'git-commit-gpt',
     description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
+      "An AI-powered Git extension that generates commit messages using OpenAI's GPT-3.5-turbo-instruct.",
+    link: {
+      href: 'https://github.com/laststance/git-commit-gpt',
+      label: 'git-commit-gpt',
+    },
+    logo: npmLogo,
   },
   {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
+    name: 'nsx',
+    description: 'Just logging tech resources I read.',
+    link: { href: 'https://github.com/laststance/nsx', label: 'nsx' },
+    logo: reactLogo,
+  },
+  {
+    name: 'React TypeScript TodoMVC 2022',
+    description: 'A Modern Code Style Todo Example.',
+    link: {
+      href: 'https://github.com/laststance/react-typescript-todomvc-2022',
+      label: 'React TypeScript TodoMVC 2022',
+    },
+    logo: reactLogo,
+  },
+  {
+    name: 'USD Query',
+    description: 'Just make $ alias for document.querySelector.',
+    link: {
+      href: 'https://github.com/laststance/usd-query',
+      label: 'USD Query',
+    },
+    logo: npmLogo,
+  },
+  {
+    name: 'nsx-browser-extension',
+    description: 'Browser Extension for nsx.',
+    link: {
+      href: 'https://github.com/laststance/nsx-browser-extension',
+      label: 'nsx-browser-extension',
+    },
+    logo: chromeLogo,
   },
 ]
 
@@ -58,6 +94,8 @@ function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
+// @TODO credit <a target="_blank" href="https://icons8.com/icon/jD-fJzVguBmw/redux">Redux</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
 export const metadata: Metadata = {
   title: 'Projects',
   description: 'Laststance.io Github repositories.',
@@ -66,8 +104,11 @@ export const metadata: Metadata = {
 export default function Projects() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      title="I eliminate all unnecessary complexity from Web Dev."
+      intro="Laststance.io is currently focusing on React and other web
+              development issues, as well as developing and researching
+              toolchains to improve the quality of software development and the
+              developer experience."
     >
       <ul className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
@@ -81,7 +122,9 @@ export default function Projects() {
               />
             </div>
             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <CardLink href={project.link.href}>{project.name}</CardLink>
+              <CardLink href={project.link.href} target="_blank">
+                {project.name}
+              </CardLink>
             </h2>
             <CardDescription>{project.description}</CardDescription>
             <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
