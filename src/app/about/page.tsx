8e8480763a6e596abe1a8d757df76782c4f8f54f
ryota-murakami <dojce1048@gmail.com>
2023-09-29 14:08:32 +0900

    Update portrait image and add new content to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/gonnahell.jpeg'
 
 export const metadata: Metadata = {
   title: 'About',
@@ -68,35 +68,40 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            Laststance.io is my experimental laboratory for eliminate like this.
+            ➡️
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I'm{' '}
+              <a
+                href="https://ryota-murakami.github.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Ryota Murakami live in Tokyo,
+              </a>
+              here is website of my personal Github organization.
+              <br />, Initially, Laststance.io was just a Github organization
+              created for use to name a NPM scoped name, such as "@foo/mylib",
+              prevent new package name already used others.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              But now it has become an important hub for me to co-locate all OSS
+              activities.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              Laststance.io is currently focusing on React and other web
+              development issues, as well as developing and researching
+              toolchains to improve the quality of software development and the
+              developer experience.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              Laststance.io final gig is making the best web application
+              framework. <br />
+              You have no time to do it within 8h 5days per week and your
+              exhausted by daily job"? Then work 3 days a week, inventing all
+              webdev/OSS things.
             </p>
           </div>
         </div>
