d7b699f69cd34981e93caa7cd005b673f8620d5f
ryota-murakami <dojce1048@gmail.com>
2023-10-07 06:53:40 +0900

    Fix styling issues in FeedItem.css and GithubFeedList.tsx

--- src/app/_home/githubFeedList.tsx
+++ src/app/_home/githubFeedList.tsx
@@ -7,7 +7,7 @@ export async function GithubFeedList() {
   const githubFeedList = await fetchGithubFeedList()
 
   return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+    <div className="w-[380px] rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
         <span className="ml-3">Github Feed</span>
