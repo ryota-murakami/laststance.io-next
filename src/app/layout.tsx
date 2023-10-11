24ba2b30443df62ce6c4d2033383a931e8466c80
ryota-murakami <dojce1048@gmail.com>
2023-10-11 09:50:28 +0900

    Add Analytics component to RootLayout in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { type Metadata } from 'next'
 
 import { Providers } from '@/app/providers'
@@ -31,6 +32,7 @@ export default function RootLayout({
             <Layout>{children}</Layout>
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
