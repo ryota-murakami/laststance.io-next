163964d5488fbd730682b97903d5c9bc4cfd3453
ryota-murakami <dojce1048@gmail.com>
2023-10-23 21:43:58 +0900

    Update postcss.config.js to use Tailwind CSS nesting plugin and add comment with link to documentation

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
-    'postcss-nesting': {},
+    'tailwindcss/nesting': {},
+    // See: https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    // eslint-disable-next-line sort-keys-custom-order/object-keys
     tailwindcss: {},
   },
 }
