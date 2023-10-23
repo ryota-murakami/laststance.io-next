dea44735ab9d5f2041c4d510b36b145e00022e4c
ryota-murakami <dojce1048@gmail.com>
2023-10-23 22:34:20 +0900

    Add new articles to sitemap and update change frequency and last modified date

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -44,5 +44,41 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.5,
       url: 'https://laststance.io/articles',
     },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: 'https://laststance.io/articles/what-is-font-family',
+    },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: 'https://laststance.io/articles/vite-trk-query-update-v0.4.0',
+    },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: 'https://laststance.io/articles/update-eslint-config-ts-prefixer@v0.23.3',
+    },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: 'https://laststance.io/articles/first-blog-post',
+    },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: 'https://laststance.io/articles/Deno-npm-compatibility-is-not-perfect-yet',
+    },
+    {
+      changeFrequency: 'weekly',
+      lastModified: new Date(),
+      priority: 0.5,
+      url: 'https://laststance.io/articles/access-nested-class-with-nodule-scss-file',
+    },
   ]
 }
