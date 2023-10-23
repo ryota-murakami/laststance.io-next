87f5d7a693748e7fc5460e102d08d747f4958682
ryota-murakami <dojce1048@gmail.com>
2023-10-24 08:08:40 +0900

    Updated ESLint rule set and React Router setup, added typography styles to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
+import typographyStyles from './typography'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./src/app/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   darkMode: ['class'],
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
   theme: {
     container: {
       center: true,
@@ -72,5 +69,21 @@ module.exports = {
         },
       },
     },
+    fontSize: {
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '3.5rem' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+    },
+    typography: typographyStyles,
   },
 }
