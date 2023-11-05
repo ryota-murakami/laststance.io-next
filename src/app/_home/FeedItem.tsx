376ecdca37fb121a093be42f688bffb1423851e3
ryota-murakami <dojce1048@gmail.com>
2023-11-05 11:16:16 +0900

    Remove unnecessary dt element from FeedItem component

--- src/app/_home/FeedItem.tsx
+++ src/app/_home/FeedItem.tsx
@@ -7,7 +7,6 @@ interface Props {
 
 export const FeedItem: React.FC<Props> = ({ feed }) => (
   <li className="FeedItem_container">
-    <dt className="sr-only">Feed Item</dt>
     <dd className="text-sm text-zinc-500 dark:text-zinc-400 dark:bg-zinc-800/90">
       <div dangerouslySetInnerHTML={{ __html: feed.content[0]._ }} />
     </dd>
