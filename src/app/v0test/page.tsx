f9e3ab5d386952d8dd24e4ac6cbcf8b6c7c5f7a2
ryota-murakami <dojce1048@gmail.com>
2023-11-20 05:10:13 +0900

    v0

---
+++ src/app/v0test/page.tsx
@@ -0,0 +1,10 @@
+import { CookieAgreement } from '../../components/cookie-agreement'
+import { SearchPanel } from '../../components/search-panel'
+
+export default function v0test() {
+  return (
+    <div className="w-[80%] m-auto pt-8 grid place-content-center">
+      <CookieAgreement />
+    </div>
+  )
+}
