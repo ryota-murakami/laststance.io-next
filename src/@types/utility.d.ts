60108a123912c0b982a11ef892925ba5d02b0901
ryota-murakami <dojce1048@gmail.com>
2023-11-20 05:21:06 +0900

    up

--- src/@types/utility.d.ts
+++ src/@types/utility.d.ts
@@ -2,16 +2,16 @@
 declare type Expand<T> = T extends (...args: infer A) => infer R
   ? (...args: Expand<A>) => Expand<R>
   : T extends infer O
-  ? { [K in keyof O]: O[K] }
-  : never
+    ? { [K in keyof O]: O[K] }
+    : never
 
 declare type ExpandRecursively<T> = T extends (...args: infer A) => infer R
   ? (...args: ExpandRecursively<A>) => ExpandRecursively<R>
   : T extends object
-  ? T extends infer O
-    ? { [K in keyof O]: ExpandRecursively<O[K]> }
-    : never
-  : T
+    ? T extends infer O
+      ? { [K in keyof O]: ExpandRecursively<O[K]> }
+      : never
+    : T
 
 declare type Year = `${d}${d}${d}${d}`
 declare type Month = `0${d}` | '10' | '11' | '12'
