fa480938427d96dd6723f52079d06455d39201a7
ryota-murakami <dojce1048@gmail.com>
2024-02-08 17:00:54 +0900

    Refactor e2e tests Consolidate desktop and mobile specs
    - Add new e2e test files for general use cases
    - Remove specific desktop and mobile e2e test files to streamline testing approach
    - Adjust project structure for better maintainability and readability of e2e tests

--- e2e/mobile/keybinds.spec.ts
+++
@@ -1,9 +0,0 @@
-import { argosScreenshot } from '@argos-ci/playwright'
-import { test, expect } from '@playwright/test'
-
-test('/keybinds', async ({ page }) => {
-  await page.goto('http://localhost:3000/keybinds')
-  await expect(page.getByText('My Editor Keybinds.')).toBeVisible()
-
-  await argosScreenshot(page, 'Mobile /keybinds')
-})
