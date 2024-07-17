0aa3cba73c9c26e104c71510e53dd1fcd19aef0e
Ryota Murakami <dojce1048@gmail.com>
2024-07-18 07:48:16 +0900

    refactor: migrate PostCSS config to ES module
    - Deleted postcss.config.js
    - Created postcss.config.mjs with the same configuration using ES module syntax

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-export const plugins = {
-  autoprefixer: {},
-  'tailwindcss/nesting': {},
-  // See: https://tailwindcss.com/docs/using-with-preprocessors#nesting
-  tailwindcss: {},
-}
---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  plugins: {
+    autoprefixer: {},
+    'tailwindcss/nesting': {},
+    // See: https://tailwindcss.com/docs/using-with-preprocessors#nesting
+    tailwindcss: {},
+  },
+}
+
+export default config
