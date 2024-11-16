6edd6edfc5c1cc6e00392fd35de4930f5faa217d
Ryota Murakami <dojce1048@gmail.com>
2024-11-16 21:06:58 +0900

    Refactor ESLint configuration
    - Removed .eslintignore and .eslintrc.cjs files.
    - Introduced eslint.config.mjs for a modern ESLint configuration setup.
    - Updated package.json scripts to use the

---
+++ eslint.config.mjs
@@ -0,0 +1,83 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import nextNext from '@next/eslint-plugin-next'
+import tsParser from '@typescript-eslint/parser'
+import jsxA11Y from 'eslint-plugin-jsx-a11y'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: [
+      '**/.vscode',
+      '**/node_modules',
+      '**/build',
+      '**/dist',
+      '**/.github',
+      '**/.git',
+      '**/.idea',
+      '**/.next',
+      '**/.husky',
+      '**/storybook-static',
+      '**/mockServiceWorker.js',
+      '**/tests-examples',
+      './playwright-report',
+      './test-results',
+      './e2e/tablet',
+      './e2e/tablet-landscape',
+    ],
+  },
+  ...compat.extends('ts-prefixer', 'plugin:jsx-a11y/recommended'),
+  {
+    plugins: {
+      '@next/next': nextNext,
+      'jsx-a11y': jsxA11Y,
+    },
+
+    languageOptions: {
+      globals: {},
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        project: ['tsconfig.json'],
+      },
+    },
+
+    settings: {},
+
+    rules: {
+      '@next/next/google-font-display': 'warn',
+      '@next/next/google-font-preconnect': 'warn',
+      '@next/next/inline-script-id': 'error',
+      '@next/next/next-script-for-ga': 'warn',
+      '@next/next/no-assign-module-variable': 'error',
+      '@next/next/no-async-client-component': 'error',
+      '@next/next/no-before-interactive-script-outside-document': 'warn',
+      '@next/next/no-css-tags': 'warn',
+      '@next/next/no-document-import-in-page': 'error',
+      '@next/next/no-duplicate-head': 'error',
+      '@next/next/no-head-element': 'warn',
+      '@next/next/no-head-import-in-document': 'error',
+      '@next/next/no-html-link-for-pages': 'warn',
+      '@next/next/no-img-element': 'warn',
+      '@next/next/no-page-custom-font': 'warn',
+      '@next/next/no-script-component-in-head': 'error',
+      '@next/next/no-styled-jsx-in-document': 'warn',
+      '@next/next/no-sync-scripts': 'warn',
+      '@next/next/no-title-in-document-head': 'warn',
+      '@next/next/no-typos': 'warn',
+      '@next/next/no-unwanted-polyfillio': 'warn',
+    },
+  },
+]
